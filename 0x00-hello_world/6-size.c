218cfec870bc051e751942ff6976968b113a33cd
Ndem Peter Adamson <piroadams@gmail.com>
2022-09-06 17:42:14 +0000

    corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,11 @@
  */
 int main(void)
 {
-int a;
-long int b;
-long long int c;
-char d;
-float f;
-
-printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("size of an int: %1u byte(s)\n", (unsigned long)sizeof(a));
-printf("size of a long int: %1u byte(s)\n", (unsigned long)sizeof(b));
-printf("size of a long long int: %1u byte(s)\n", (unsigned long)sizeof(c));
-printf("size of a float: %1u byte(s)\n", (unsigned long)sizeof(f));
-return (0);
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %1d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %1d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %1d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %1d byte(s)\n", sizeof(float));
+	return (0);
 }
 
