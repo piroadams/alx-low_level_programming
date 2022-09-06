0aa844119bd298669113e11a20b82f9d77624233
Ndem Peter Adamson <piroadams@gmail.com>
2022-09-06 17:52:03 +0000

    corrections 2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Sucess)
- */
+ * main - entry point
+ * Description: Print a string using followed by a new line
+ * Return: 0
+*/
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
-
