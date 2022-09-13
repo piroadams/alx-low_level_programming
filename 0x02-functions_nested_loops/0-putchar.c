2832cb1931f193d0ba32d145e1ff2d607ad906c7
Ndem Peter Adamson <piroadams@gmail.com>
2022-09-13 16:26:54 +0000

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = "Task 0";
+
+	while (ch)
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,45 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+
+/* TASK 0 */
+int _putchar(char c);
+
+/* TASK 1 */
+void print_alphabet(void);
+
+/* TASK 2 */
+void print_alphabet_x10(void);
+
+/* TASK 3 */
+int _islower(int c);
+
+/* TASK 4 */
+int _isalpha(int c);
+
+/* TASK 5 */
+int print_sign(int n);
+
+/* TASK 6 */
+int _abs(int);
+
+/* TASK 7 */
+int print_last_digit(int);
+
+/* TASK 8 */
+void jack_bauer(void);
+
+/* TASK 9 */
+void times_table(void);
+
+/* TASK 10 */
+int add(int, int);
+
+/* TASK 11 */
+void print_to_98(int n);
+
+/* TASK 12 */
+void print_times_table(int n);
+
+#endif /* MAIN_H */
