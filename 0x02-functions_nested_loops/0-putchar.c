62c140e562a61711a6c213429eca4fc70243f95a
Ndem Peter Adamson <piroadams@gmail.com>
2022-09-13 22:55:01 +0000

    Corrections

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,20 @@
 #include "main.h"
-
 /**
- * main - Entry point
- *
+ * main - Entry 
+ * Description: You are not allowed to include standard libraries.
  * Return: 0
  */
 int main(void)
 {
-	_putchar('m');
-	_putchar('e');
-	_printf("\n");
-	return(0);
+	char name[] = "Adamson";
+	int i = 0;
+
+	while (name[i] != '\0')
+	{
+		_putchar(name[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
 }
