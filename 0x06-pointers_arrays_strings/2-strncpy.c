a2b47f294021d23d013ac3a350b05bd7fa45b15b
Ndem Peter Adamson <piroadams@gmail.com>
2022-09-21 23:06:57 +0000

    2-strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,32 +1,33 @@
 #include "main.h"
 
 /**
- * _strncpy - a function that copys the strings.
- * @dest: an input string
- * @src: an input string
- * @n: an input integer
- * Return: A pointer to the resulting string
+ * _strncpy - copy a string
+ * @dest: the buffer to copy to
+ * @src: the string to copy
+ * @n: the maximum number of bytes to copy from src
+ *
+ * Description: This function copies at most n bytes of the string pointed
+ * to by src to the buffer pointed to by dest. If the length of src is less
+ * than n, this function will write null bytes until a n bytes are wrtten.
+ *
+ * Return: a pointer to the destination string dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int srclen = 0, i = 0;
-	char *temp = dest, *start = src;
+	char *dest_end = dest;
+	int src_len = 0;
 
-	while (*src)
+	while (src_len < n && src[src_len])
+		++src_len;
+
+	if (src + src_len < dest || dest + n < src)
 	{
-		srclen++;
-		src++;
+		for (; src_len; --src_len, --n)
+			*dest_end++ = *src++;
+
+		for (; n; --n)
+			*dest_end++ = '\0';
 	}
 
-	srclen++;
-
-	if (n > srclen)
-		n = srclen;
-
-	src = start;
-
-	for (; i < n; i++)
-		*dest++ = *src++;
-
-	return (temp);
-}
+	return (dest);
+} 
