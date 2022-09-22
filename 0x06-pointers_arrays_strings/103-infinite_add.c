aa9f7231d2965e5e3a3a409f550ac434d0cc57b9
Ndem Peter Adamson <piroadams@gmail.com>
2022-09-22 16:55:11 +0000

    Infinite add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,85 +1,66 @@
-#include "main.h"
-
 /**
- * carry_from_prev - carry overflow from previous index to current index
- * @b: the buffer to operate on
- * @i: the index to carry to
+ * infinite_add - Add up two numbers stored in given char arrays
+ * @n1: The first number
+ * @n2: The second number
+ * @r: Pointer to the buffer to store result
+ * @size_r: The size of the buffer
  *
- * Return: void
- */
-void carry_from_prev(char *b, int i)
-{
-	b[i] = b[i - 1] / 10;
-	b[i - 1] %= 10;
-}
-
-
-/**
- * find_str_end - find the end of a string
- * @s: the string to search
- *
- * Return: a pointer to the end of the string
- */
-char *find_str_end(char *s)
-{
-	char *s_end;
-
-	for (s_end = s; *s_end; ++s_end)
-		;
-	return (s_end);
-}
-
-
-/**
- * infinite_add - adds two numbers
- * @n1: the first number to add
- * @n2: the other number to add
- * @r: the buffer to store the result
- * @size_r: the size of the buffer
- *
- * Return: a pointer to the buffer r, unless the result does not fit into r,
- * in which case return 0.
+ * Return: 0 if buffer too small to store result, else return pointer to buffer
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	char *n1_end, *n2_end;
-	int i, j;
+	int l1, l2, tmpl, rl, i, sum, num1, num2, carry;
+	char tmp[10000];
 
-	for ( ; n1[1] && *n1 == '0'; ++n1)
-		;
-	for ( ; n2[1] && *n2 == '0'; ++n2)
-		;
-	n1_end = find_str_end(n1);
-	n2_end = find_str_end(n2);
-	for (i = 0; n1 < n1_end || n2 < n2_end; ++i)
+	rl = i = l1 = l2 = sum = num1 = num2 = carry = 0;
+	while (n1[l1] != '\0')
+		l1++;
+	while (n2[l2] != '\0')
+		l2++;
+	if (l1 + 2 > size_r || l2 + 2 > size_r)
+		return (0);
+	l1--;
+	l2--;
+	while (i <= l1 || i <= l2)
 	{
-		if (i + 1 >= size_r)
-			return (0);
-		if (i && r[i - 1] > 9)
-			carry_from_prev(r, i);
+		num1 = num2 = 0;
+		if (i <= l1)
+			num1 = n1[l1 - i] - '0';
+		if (i <= l2 && (l2 - i) >= 0)
+			num2 = n2[l2 - i] - '0';
+		sum = num1 + num2 + carry;
+		if (sum >= 10)
+		{
+			carry = 1;
+			sum -= 10;
+		}
 		else
-			r[i] = 0;
-		if (n1 < n1_end)
-			r[i] += *(--n1_end) - '0';
-		if (n2 < n2_end)
-			r[i] += *(--n2_end) - '0';
+			carry = 0;
+		r[i] = sum + '0';
+		i++;
+		rl++;
 	}
-	if (r[i - 1] > 9)
+	if (carry > 0)
 	{
-		if (i + 1 >= size_r)
-			return (0);
-		carry_from_prev(r, i++);
+		r[i] = carry + '0';
+		r[i + 1] = '\0';
 	}
-	r[i--] = '\0';
-	for (j = 0; j < i; --i, ++j)
+	i = tmpl = 0;
+	while (i <= rl)
 	{
-		r[i] ^= r[j];
-		r[j] ^= r[i];
-		r[i] ^= r[j];
-		r[i] += '0';
-		r[j] += '0';
+		tmp[i] = r[rl - i];
+		tmpl++;
+		i++;
 	}
-	if (j == i)
-		r[i] += '0';
+	i = 0;
+	while (i < tmpl)
+	{
+		if (r[i] == '\0')
+		{
+			break;
+		}
+		r[i] = tmp[i];
+		i++;
+	}
 	return (r);
 }
